53b96f3e54fd4fbd2fed7440d236aa4ad99b183e
sampathchowdary <sampathgundapuneni@gmail.com>
2023-09-14 12:37:12 -0500

    basepath updated

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const App = () => {
   const titleRef = React.useRef();
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={"/home"}>
       {navBar.show && <Navbar ref={titleRef} />}
       <Routes>
         <Route path="/" exact element={<Home ref={titleRef} />} />
